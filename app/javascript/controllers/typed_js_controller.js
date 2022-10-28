ab8550ed5f62b9f41685c59533818e76453f61f3
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-10-28 15:54:04 +0200

    Refactor banner_show and card_category

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,6 +4,7 @@ import Typed from "typed.js"
 // Connects to data-controller="typed-js"
 export default class extends Controller {
   connect() {
+    console.log()
     new Typed(this.element, {
       strings: ["Action", "Comedy", "Drama", "Fantasy"],
       typeSpeed: 70,
